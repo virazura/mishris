60762364347bfa50ffc9948e9d227c569fe68da5
firazura <guest@localhost.localdomain>
2019-06-28 14:55:26 +0700

    fix hierarchy data

--- mishris/www/hierarchy.py
+++ mishris/www/hierarchy.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 import frappe
+import json
 
 
 def get_context(context):
-    context.employee = frappe.get_list("Employee", fields=["employee_name", "designation"])
+    employee_data = frappe.get_list("Employee", fields=["employee_name", "designation", "employee_name_for_report"])
+    context["employee_data"] = json.dumps(employee_data)
+
+    
+    
     
 
 
